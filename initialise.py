903551ce40dbdbafd1943edfd5fc4219e6bc0aa1
laerson-hammes <laersonha@gmail.com>
2021-04-30 17:42:34 -0300

    Changes

--- initialise.py
+++ initialise.py
@@ -3,11 +3,11 @@ import argparse
 
 
 class RequirementsGenerator(object):
-   def __init__(self, file_name: str) -> None:
-      self.file_name: str = file_name
+   def __init__(self, /) -> None:
+      self.run()
 
 
-   def get_version(self, packets: list[str]) -> dict:
+   def get_version(self, packets: list[str], /) -> dict:
       result: dict = {}
       for packet in packets:
          try:
@@ -17,7 +17,7 @@ class RequirementsGenerator(object):
       return result
 
 
-   def read_file(self, file_name: str) -> list[str]:
+   def read_file(self, file_name: str, /) -> list[str]:
       packets: list[str] = []
       try:
          with open(file_name, "r") as f:
@@ -28,7 +28,7 @@ class RequirementsGenerator(object):
          raise Exception("File not found error...")
          
 
-   def requirements_file_generator(self, packets: dict) -> None:
+   def requirements_file_generator(self, packets: dict, /) -> None:
       with open("requirements.txt", "w") as f:
          for index, packet in enumerate(packets):
             final: str  = "\n"
@@ -37,22 +37,21 @@ class RequirementsGenerator(object):
             f.write(f"{packet}={packets[packet] + final}")
 
 
-   def run(self) -> None:
-      packets: list[str] = self.read_file(file_name)
+   def run(self, /) -> None:
+      packets: list[str] = self.read_file(self.get_arguments())
       packets_version: dict = self.get_version(packets)
       self.requirements_file_generator(packets_version)
 
 
-def get_arguments() -> str:
-   parser = argparse.ArgumentParser()
-   parser.add_argument("-f", "--file", dest="file", help="File name with the name of the libraries")
-   options = parser.parse_args()
-   if not options.file:
-      file_name: str = str(input("Enter the name of the file with the name of the libraries: "))
-      return file_name
-   return options.file
+   def get_arguments(self, /) -> str:
+      parser = argparse.ArgumentParser()
+      parser.add_argument("-f", "--file", dest="file", help="File name with the name of the libraries")
+      options = parser.parse_args()
+      if not options.file:
+         file_name: str = str(input("Enter the name of the file with the name of the libraries: "))
+         return file_name
+      return options.file
 
 
 if __name__ == "__main__":
-   generator = RequirementsGenerator(file_name := get_arguments())
-   generator.run()
+   generator = RequirementsGenerator()
